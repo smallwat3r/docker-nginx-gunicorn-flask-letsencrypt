45d347485d47226539ae80659a74e33593dc05af
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2019-02-15 15:14:26 +0000

    Load gunicorn config from file.

--- Makefile
+++ Makefile
@@ -3,10 +3,9 @@ dc-start:
 	@docker-compose build;
 	@docker-compose up -d;
 
-dc-restart:
-	@docker-compose stop;
-	@docker-compose build;
-	@docker-compose up -d;
-
 dc-stop:
 	@docker-compose stop;
+
+dc-cleanup:
+	@docker rm $(shell docker ps -qa --no-trunc --filter "status=exited")
+	@docker rmi $(shell docker images --filter "dangling=true" -q --no-trunc)
---
+++ core/gunicorn_ini.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : gunicorn_ini.py
+# Author            : Matthieu Petiteau <mpetiteau.pro@gmail.com>
+# Date              : Fri 15 Feb 12:02:16 2019
+# Last Modified Date: Fri 15 Feb 12:02:16 2019
+# Last Modified By  : Matthieu Petiteau <mpetiteau.pro@gmail.com>
+
+"""Init gunicorn config."""
+
+import multiprocessing
+
+
+bind = '0.0.0.0:5000'
+workers = multiprocessing.cpu_count() * 2 + 1
