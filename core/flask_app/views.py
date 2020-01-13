1d97d62c9ac7ee6293e4fca35e881e8d706d2970
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-01-13 14:20:46 +0000

    Updates

--- core/flask_app/views.py
+++ core/flask_app/views.py
@@ -4,27 +4,29 @@
 # Author: Matthieu Petiteau <mpetiteau.pro@gmail.com>
 # Date  : 27.12.2019
 
-'''Views.'''
+"""Flask application views."""
 from flask import render_template, request, send_from_directory
 
 from . import app
 
 
-@app.route('/')
+@app.route("/")
 def root():
-    '''Main.'''
-    msg = ('Congrats, you\'re running Flask, Gunicorn, Nginx and LetsEncrypt'
-           ' using Docker containers')
-    return render_template('index.html', msg=msg)
+    """Main."""
+    msg = (
+        "Congrats, you're running Flask, Gunicorn, Nginx and LetsEncrypt"
+        " using Docker containers"
+    )
+    return render_template("index.html", msg=msg)
 
 
-@app.route('/robots.txt')
+@app.route("/robots.txt")
 def static_from_root():
-    '''Serve robots.txt file.'''
+    """Serve robots.txt file."""
     return send_from_directory(app.static_folder, request.path[1:])
 
 
 @app.errorhandler(404)
 def error_404(error):
-    '''404 error handling.'''
-    return render_template('404.html', error=error)
+    """404 error handling."""
+    return render_template("404.html", error=error)
