4bcd9d5da9d6e5f901d9125df619a67f8db36acb
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2020-12-25 01:48:11 +0100

    Remove Gunicorn config file

--- src/gunicorn.ini.py
+++
@@ -1,7 +0,0 @@
-# Gunicorn configuration file.
-# This file can be used from the Gunicorn cli with the ``-c`` paramater.
-# Eg. ``gunicorn -c <config_file>``
-import multiprocessing
-
-bind = "0.0.0.0:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
