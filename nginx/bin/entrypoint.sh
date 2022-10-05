5c5f240340f2ec6d922b8018cfb35612606a60df
Fabio Merola <fabio.merola95@gmail.com>
2022-10-05 18:48:42 +0200

    fix on ssl certs auto-renew

--- nginx/bin/entrypoint.sh
+++ nginx/bin/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 certbot certonly --standalone -d $1 --email $2 -n --agree-tos --expand
-/usr/sbin/nginx -g "daemon off;"
-/usr/sbin/crond -f -d 8 &
+/usr/sbin/crond -f &
+/usr/sbin/nginx -g "daemon off;" #this call is blocking so it should be in last line
