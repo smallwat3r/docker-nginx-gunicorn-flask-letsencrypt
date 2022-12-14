20d69593f2dad65d71097b359b54333700f482bb
Fabio Merola <fabio.merola95@gmail.com>
2022-12-14 11:24:52 +0100

    Update nginx/bin/entrypoint.sh
    Co-authored-by: Matthieu Petiteau <matt@smallwat3r.com>

--- nginx/bin/entrypoint.sh
+++ nginx/bin/entrypoint.sh
@@ -2,4 +2,4 @@
 
 certbot certonly --standalone -d $1 --email $2 -n --agree-tos --expand
 /usr/sbin/crond -f &
-/usr/sbin/nginx -g "daemon off;" #this call is blocking so it should be in last line
+/usr/sbin/nginx -g "daemon off;"  # this is run in the foreground, so need to be last
