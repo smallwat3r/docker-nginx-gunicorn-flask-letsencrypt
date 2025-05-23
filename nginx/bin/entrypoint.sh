56fe967539d2cccf65a311beeacc1b6f2bff8139
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-05-23 11:24:01 +0100

    Use envsubst in entrypoint

--- nginx/bin/entrypoint.sh
+++ nginx/bin/entrypoint.sh
@@ -3,5 +3,10 @@
 : "${DOMAIN:?DOMAIN is not set or is empty}"
 : "${EMAIL:?EMAIL is not set or is empty}"
 
+# use envsubst to fill placeholders in the Nginx app config file,
+# see nginx/Dockerfile for more details about what this does.
+envsubst </etc/nginx/conf.d/app.conf >/etc/nginx/conf.d/app.subst.conf &&
+  rm /etc/nginx/conf.d/app.conf
+
 certbot certonly --standalone -d "${DOMAIN}" --email "${EMAIL}" -n --agree-tos --expand
 /usr/sbin/crond && /usr/sbin/nginx -g "daemon off;"
